d87d71b3f3e6d44537921beffc8de8a267bd6097
Anna Kowalczuk <anna@kowalczuk.eu>
2018-11-04 23:29:24 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+  res.render('main');
+});
+
+app.get('/auth/google', function(req, res){
+  res.render('login');
+});
+
+app.post('/userform', function (req, res) {
+  // const response = {
+  //     username: req.query.username,
+  //     password: req.query.password
+  // };
+  // res.end(JSON.stringify(response));
+  var filePath = "assets/cat.jpg"
+  var resolvedPath = path.resolve(filePath);
+  console.log(resolvedPath);
+  res.sendFile(resolvedPath);
+});
+
+app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
